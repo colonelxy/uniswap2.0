887969ebbff4b6634065ee057497e7eb95c96d24
colonelxy <harold2@protonmail.com>
2022-09-12 13:38:22 +0300

    first commit

---
+++ packages/react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { useEthers } from '@usedapp/core';
+
+import styles from './styles';
+import { uniswapLogo } from './assets';
+
+const App = () => {
+  return <p>Hello, Web3!</p>
+}
+
+export default App;
